ad800f3111625c1e84556eaabf99840b2f5960f0
SHELKE AKSHAY NANDKUMAR - 2022mt93331 <2022mt93331@wilp.bits-pilani.ac.in>
2023-10-27 16:01:29 +0530

    assignment code

---
+++ SQL_FILE/library_management_system.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2021 at 05:28 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library_managment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `pass` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `pass`) VALUES
+(1, 'admin@gmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
+  `bookpic` varchar(500) NOT NULL,
+  `bookname` varchar(500) NOT NULL,
+  `bookdetail` varchar(500) NOT NULL,
+  `bookaudor` varchar(500) NOT NULL,
+  `bookpub` varchar(500) NOT NULL,
+  `branch` varchar(500) NOT NULL,
+  `bookprice` varchar(500) NOT NULL,
+  `bookquantity` varchar(500) NOT NULL,
+  `bookava` int(11) NOT NULL,
+  `bookrent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`id`, `bookpic`, `bookname`, `bookdetail`, `bookaudor`, `bookpub`, `branch`, `bookprice`, `bookquantity`, `bookava`, `bookrent`) VALUES
+(1, '1.jpg', 'Fundamentals of Database System', '7th edition, 2017', 'Ramez Elamsri and Shamkant Navathe', 'Pearson Education', 'IT', '490', '100', 100, 0),
+(2, '2.jpg', 'Database System Concepts', '6th edition, 2013', 'Abraham Silberschatz and Henry F Korth and S Sudarshan', 'McGraw Hill', 'IT', '264', '100', 100, 0),
+(3, '3.jpg', 'An Introduction to Database Systems', '8th edition, 2006', 'Christopher J. Date', 'Addison Wesley', 'IT', '579', '100', 100, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issuebook`
+--
+
+CREATE TABLE `issuebook` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `issuename` varchar(500) NOT NULL,
+  `issuebook` varchar(500) NOT NULL,
+  `issuetype` varchar(500) NOT NULL,
+  `issuedays` int(11) NOT NULL,
+  `issuedate` varchar(500) NOT NULL,
+  `issuereturn` varchar(500) NOT NULL,
+  `fine` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `issuebook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requestbook`
+--
+
+CREATE TABLE `requestbook` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL,
+  `username` varchar(500) NOT NULL,
+  `usertype` varchar(500) NOT NULL,
+  `bookname` varchar(500) NOT NULL,
+  `issuedays` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdata`
+--
+
+CREATE TABLE `userdata` (
+  `id` int(11) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` varchar(500) NOT NULL,
+  `type` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userdata`
+--
+
+INSERT INTO `userdata` (`id`, `name`, `email`, `pass`, `type`) VALUES
+(1, 'akshay_shelke', '2022mt93331@wilp.bits-pilani.ac.in', '123', 'student'),
+(2, 'john_doe', 'johndoe@gmail.com', '123', 'student');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `issuebook`
+--
+ALTER TABLE `issuebook`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pk_fk` (`userid`);
+
+--
+-- Indexes for table `requestbook`
+--
+ALTER TABLE `requestbook`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pk_fk_book` (`bookid`),
+  ADD KEY `pk_fk_users` (`userid`);
+
+--
+-- Indexes for table `userdata`
+--
+ALTER TABLE `userdata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `issuebook`
+--
+ALTER TABLE `issuebook`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `requestbook`
+--
+ALTER TABLE `requestbook`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `userdata`
+--
+ALTER TABLE `userdata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `issuebook`
+--
+ALTER TABLE `issuebook`
+  ADD CONSTRAINT `pk_fk` FOREIGN KEY (`userid`) REFERENCES `userdata` (`id`);
+
+--
+-- Constraints for table `requestbook`
+--
+ALTER TABLE `requestbook`
+  ADD CONSTRAINT `pk_fk_users` FOREIGN KEY (`userid`) REFERENCES `userdata` (`id`);
+COMMIT;
+
+ALTER TABLE `requestbook`
+  ADD CONSTRAINT `pk_fk_book` FOREIGN KEY (`bookid`) REFERENCES `book` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
